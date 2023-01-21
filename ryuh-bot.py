3724f59161488c352a3fb7e43406f860f1a4a2c5
Preston Lo <prestonloz97@gmail.com>
2023-01-21 15:20:15 +0000

    file restructure

--- ryuh-bot.py
+++ ryuh-bot.py
@@ -10,17 +10,10 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 import discord
 from discord.ext import commands
-from datetime import date, timedelta, datetime
-import pytz
+import scheduler
+import users
+import channels
 
-# datetime.datetime.today().weekday()
-# output:
-# 0 = Monday
-# 6 = Sunday
-#
-# date.today().strftime("%d/%b/%y")
-# date format = dd/Jan/23
-#
 # if you get error 
 # 'NoneType' object has no attribute 'fetch_message'
 # code example:
@@ -32,148 +25,9 @@ import pytz
 # Stackoverflow: It seems like you are trying to call a bot's functionality before actually running your bot.
 # Try to add your code inside on_ready() callback to ensure that you are trying to get your channel only after initializing the bot itself.
 
-# not sure if this works, that is, i scare the .weekday() still uses default timezone
-# this can be seen if you datetime.now(ytz.timezone('Asia/Singapore')).today()
-# perhaps .today() is similar to .now() and it overwrites
-my_timezone = pytz.timezone('Asia/Singapore')
-now = datetime.now()
-today_weekday = now.weekday()
-first_day_of_week_offset = -today_weekday
-
-monday = now + timedelta(days=first_day_of_week_offset+7)
-monday = monday.strftime("%d/%b/%y")
-first_day_of_week_offset+=1
-
-tuesday = now + timedelta(days=first_day_of_week_offset+7)
-tuesday = tuesday.strftime("%d/%b/%y")
-first_day_of_week_offset+=1
-
-wednesday = now + timedelta(days=first_day_of_week_offset+7)
-wednesday = wednesday.strftime("%d/%b/%y")
-first_day_of_week_offset+=1
-
-thursday = now + timedelta(days=first_day_of_week_offset)
-thursday = thursday.strftime("%d/%b/%y")
-first_day_of_week_offset+=1
-
-friday = now + timedelta(days=first_day_of_week_offset)
-friday = friday.strftime("%d/%b/%y")
-first_day_of_week_offset+=1
-
-saturday = now + timedelta(days=first_day_of_week_offset)
-saturday = saturday.strftime("%d/%b/%y")
-first_day_of_week_offset+=1
-
-sunday = now + timedelta(days=first_day_of_week_offset)
-sunday = sunday.strftime("%d/%b/%y")
-
-mon_10_pm = "10pm - 🐠"
-mon_11_pm = "11pm - 🐟"
-tue_10_pm = "10pm - 🐬"
-tue_11_pm = "11pm - 🐳"
-wed_10_pm = "10pm - 🐙"
-
-thu_10_pm = "10pm - 🐱"
-thu_11_pm = "11pm - 🐶"
-fri_10_pm = "10pm - 🐰"
-fri_11_pm = "11pm - 🐹"
-fri_12_am = "12am - 🐻"
-sat_10_pm = "10pm - 🐯"
-sat_11_pm = "11pm - 🦁"
-sat_12_am = "12am - 🐼"
-sun_10_pm = "10pm - 🐷"
-sun_11_pm = "11pm - 🐮"
-
-all_cannot = "all cannot - 🙃"
-
-
-schedule_weekend_msg = '''\
-Thursday Night - **{thursday}**
-
-{thu_10_pm}
-{thu_11_pm}
-
-Friday Night - **{friday}**
-
-{fri_10_pm}
-{fri_11_pm}
-{fri_12_am}
-
-Saturday Night - **{saturday}**
-
-{sat_10_pm}
-{sat_11_pm}
-{sat_12_am}
-
-Sunday Night - **{sunday}**
-
-{sun_10_pm}
-{sun_11_pm}
-{all_cannot}\
-'''.format( thursday=thursday, 
-            friday=friday, 
-            saturday=saturday, 
-            sunday=sunday,
-            thu_10_pm=thu_10_pm,
-            thu_11_pm=thu_11_pm,
-            fri_10_pm=fri_10_pm,
-            fri_11_pm=fri_11_pm,
-            fri_12_am=fri_12_am,
-            sat_10_pm=sat_10_pm,
-            sat_11_pm=sat_11_pm,
-            sat_12_am=sat_12_am,
-            sun_10_pm=sun_10_pm,
-            sun_11_pm=sun_11_pm,
-            all_cannot=all_cannot)
-
-schedule_weekday_msg = '''\
-Monday Night - **{monday}**
-
-{mon_10_pm}
-{mon_11_pm}
-
-Tuesday Night - **{tuesday}**
-
-{tue_10_pm}
-{tue_11_pm}
-
-Wednesday Night - **{wednesday}**
-
-{wed_10_pm}
-{all_cannot}\
-'''.format( monday=monday, 
-            tuesday=tuesday,
-            wednesday=wednesday,
-            mon_10_pm=mon_10_pm,
-            mon_11_pm=mon_11_pm,
-            tue_10_pm=tue_10_pm,
-            tue_11_pm=tue_11_pm,
-            wed_10_pm=wed_10_pm,
-            all_cannot=all_cannot)
-
 # default
 intents = discord.Intents.default()
 intents.message_content = True
-
-
-# Jumping Sushi hboss-sellherbs channel
-js_hboss_channel_id = 963160372385296414
-
-# my #general channel
-my_gen_channel_id = 803958155935219724
-
-# This is nested dictionary
-# To get emoji, upload emoji in ur server, type "\:<emoji name>:"
-# discord will output <:emoji:emoji_id>
-users_dict = {
-    'hwangz#8075'           : { 'name' : 'hwangz' , 'id' : '<@490507365704138763>' , 'emoji' : '<:hwangz:1065984480860446781>'},
-    'Fruit#8143'            : { 'name' : 'kong'   , 'id' : '<@274075256275206145>' , 'emoji' : '<:kong:1065984511290134568>'},
-    'FloatLikeBubble#0529'  : { 'name' : 'tele'   , 'id' : '<@311477931576918016>' , 'emoji' : '<:tele:1065984513215303700>'},
-    'Jasmine#4582'          : { 'name' : 'jazz'   , 'id' : '<@389193536043483138>' , 'emoji' : '<:jazz:1065984516734328832>'},
-    'LL#2409'               : { 'name' : 'ryuh'   , 'id' : '<@702529999068200970>' , 'emoji' : '<:ryuh:1065984507980812320>'},
-    'kevinc#3600'           : { 'name' : 'kevin'  , 'id' : '<@131389918998953985>' , 'emoji' : '<:farty:1065984505934000249>'},
-    # 'clem#1138'             : { 'name' : 'clem'   , 'id' : '<@304579645003530251>' , 'emoji' : '<:clem:1065984502343663637>'},
-}
 
 # https://stackoverflow.com/questions/73393567/discord-py-client-run-and-bot-run-in-one-code
 # client = discord.Client(intents=intents)
@@ -184,7 +38,7 @@ client = commands.Bot(command_prefix='!', intents=intents)
 async def check(ctx, arg):
     # you have to use .copy()
     # else anything u chg on _temp will affect on the ori dict also
-    users_dict_temp = users_dict.copy()
+    users_dict_temp = users.users_dict.copy()
     message = ''
     # Have to do this, else you get error channel has no attribute 'fetch_message'
     cur_ch_id = ctx.channel.id
@@ -259,8 +113,8 @@ async def check(ctx, arg):
 
         message += " : "
         async for user in reaction.users():
-            if(str(user) in users_dict):
-                message += users_dict[str(user)]['emoji']
+            if(str(user) in users.users_dict):
+                message += users.users_dict[str(user)]['emoji']
             # if is bot itself, dont add the blank emoji
             elif(user == client.user):
                 continue
@@ -302,9 +156,9 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('ryuh weekend'):
         members = ''
-        msg_sent = await message.channel.send(schedule_weekend_msg)
+        msg_sent = await message.channel.send(scheduler.schedule_weekend_msg)
         msg_id = msg_sent.id
-        if(message.channel.id == js_hboss_channel_id):
+        if(message.channel.id == channels.js_hboss_channel_id):
             f = open("last_scheduled_msg_id.txt", "w")
             f.write(str(msg_id))
             f.close()
@@ -320,7 +174,7 @@ async def on_message(message):
         await msg_to_react.add_reaction("🐷")
         await msg_to_react.add_reaction("🐮")
         await msg_to_react.add_reaction("🙃")
-        for dis_tag, dis_info in users_dict.items():
+        for dis_tag, dis_info in users.users_dict.items():
             for key in dis_info:
                 if(key == 'id'):
                     members += dis_info[key] # To append all mention ID into one single variable
@@ -330,9 +184,9 @@ async def on_message(message):
 
     if message.content.startswith('ryuh weekday'):
         members = ''
-        msg_sent = await message.channel.send(schedule_weekday_msg)
+        msg_sent = await message.channel.send(scheduler.schedule_weekday_msg)
         msg_id = msg_sent.id
-        if(message.channel.id == js_hboss_channel_id):
+        if(message.channel.id == channels.js_hboss_channel_id):
             f = open("last_scheduled_msg_id.txt", "w")
             f.write(str(msg_id))
             f.close()
@@ -343,7 +197,7 @@ async def on_message(message):
         await msg_to_react.add_reaction("🐳")
         await msg_to_react.add_reaction("🐙")
         await msg_to_react.add_reaction("🙃")
-        for dis_tag, dis_info in users_dict.items():
+        for dis_tag, dis_info in users.users_dict.items():
             for key in dis_info:
                 if(key == 'id'):
                     members += dis_info[key]
---
+++ scheduler.py
@@ -0,0 +1,146 @@
+from datetime import date, timedelta, datetime
+import pytz
+
+# Resets every monday
+# if you call it on monday, the schedule on monday will be next week's one
+
+# Bossing resets on Thursday 12am
+# So, mon, tues, wed date calculation will be next week's
+# Thus, fri, sat, sun will be this week's date
+
+# datetime.datetime.today().weekday()
+# output:
+# 0 = Monday
+# 6 = Sunday
+#
+# date.today().strftime("%d/%b/%y")
+# date format = dd/Jan/23
+
+# not sure if this works, that is, i scare the .weekday() still uses default timezone
+# this can be seen if you datetime.now(ytz.timezone('Asia/Singapore')).today()
+# perhaps .today() is similar to .now() and it overwrites
+
+my_timezone = pytz.timezone('Asia/Singapore')
+now = datetime.now()
+today_weekday = now.weekday()
+# Get negative value, for calculation
+first_day_of_week_offset = -today_weekday
+
+# days=first_day_of_week_offset+7 == next monday
+monday = now + timedelta(days=first_day_of_week_offset+7)
+monday = monday.strftime("%d/%b/%y")
+first_day_of_week_offset+=1
+
+# days=first_day_of_week_offset+7 == next tuesday
+tuesday = now + timedelta(days=first_day_of_week_offset+7)
+tuesday = tuesday.strftime("%d/%b/%y")
+first_day_of_week_offset+=1
+
+# days=first_day_of_week_offset+7 == next wednesday
+wednesday = now + timedelta(days=first_day_of_week_offset+7)
+wednesday = wednesday.strftime("%d/%b/%y")
+first_day_of_week_offset+=1
+
+# days=first_day_of_week_offset == this week's thursday
+thursday = now + timedelta(days=first_day_of_week_offset)
+thursday = thursday.strftime("%d/%b/%y")
+first_day_of_week_offset+=1
+
+# days=first_day_of_week_offset == this week's friday
+friday = now + timedelta(days=first_day_of_week_offset)
+friday = friday.strftime("%d/%b/%y")
+first_day_of_week_offset+=1
+
+# days=first_day_of_week_offset == this week's saturday
+saturday = now + timedelta(days=first_day_of_week_offset)
+saturday = saturday.strftime("%d/%b/%y")
+first_day_of_week_offset+=1
+
+# days=first_day_of_week_offset == this week's sunday
+sunday = now + timedelta(days=first_day_of_week_offset)
+sunday = sunday.strftime("%d/%b/%y")
+
+
+mon_10_pm = "10pm - 🐠"
+mon_11_pm = "11pm - 🐟"
+tue_10_pm = "10pm - 🐬"
+tue_11_pm = "11pm - 🐳"
+wed_10_pm = "10pm - 🐙"
+
+thu_10_pm = "10pm - 🐱"
+thu_11_pm = "11pm - 🐶"
+fri_10_pm = "10pm - 🐰"
+fri_11_pm = "11pm - 🐹"
+fri_12_am = "12am - 🐻"
+sat_10_pm = "10pm - 🐯"
+sat_11_pm = "11pm - 🦁"
+sat_12_am = "12am - 🐼"
+sun_10_pm = "10pm - 🐷"
+sun_11_pm = "11pm - 🐮"
+
+all_cannot = "all cannot - 🙃"
+
+
+schedule_weekend_msg = '''\
+Thursday Night - **{thursday}**
+
+{thu_10_pm}
+{thu_11_pm}
+
+Friday Night - **{friday}**
+
+{fri_10_pm}
+{fri_11_pm}
+{fri_12_am}
+
+Saturday Night - **{saturday}**
+
+{sat_10_pm}
+{sat_11_pm}
+{sat_12_am}
+
+Sunday Night - **{sunday}**
+
+{sun_10_pm}
+{sun_11_pm}
+{all_cannot}\
+'''.format( thursday=thursday, 
+            friday=friday, 
+            saturday=saturday, 
+            sunday=sunday,
+            thu_10_pm=thu_10_pm,
+            thu_11_pm=thu_11_pm,
+            fri_10_pm=fri_10_pm,
+            fri_11_pm=fri_11_pm,
+            fri_12_am=fri_12_am,
+            sat_10_pm=sat_10_pm,
+            sat_11_pm=sat_11_pm,
+            sat_12_am=sat_12_am,
+            sun_10_pm=sun_10_pm,
+            sun_11_pm=sun_11_pm,
+            all_cannot=all_cannot)
+
+schedule_weekday_msg = '''\
+Monday Night - **{monday}**
+
+{mon_10_pm}
+{mon_11_pm}
+
+Tuesday Night - **{tuesday}**
+
+{tue_10_pm}
+{tue_11_pm}
+
+Wednesday Night - **{wednesday}**
+
+{wed_10_pm}
+{all_cannot}\
+'''.format( monday=monday, 
+            tuesday=tuesday,
+            wednesday=wednesday,
+            mon_10_pm=mon_10_pm,
+            mon_11_pm=mon_11_pm,
+            tue_10_pm=tue_10_pm,
+            tue_11_pm=tue_11_pm,
+            wed_10_pm=wed_10_pm,
+            all_cannot=all_cannot)
