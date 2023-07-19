41268b1b8e0b6acc787c8f7bff9737c04d816a9f
Preston Lo <prestonloz97@gmail.com>
2023-07-19 06:59:28 +0000

    change to build python executable file with custom name

--- make.sh
+++ make.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # make executable file
-pyinstaller --onefile main.py
+pyinstaller --name main.out --onefile main.py
 # cd into output folder
 cd dist/
 # write my current version into text.file
 echo $(git describe) >| VERSION.md
 cd ../
 # move the executable file out to current directory
-mv dist/main .
+mv dist/main.out .
 # run it
-./main
+./main.out
